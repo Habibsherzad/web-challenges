fec57e4c7e0fcb640027daca06bd25a3002621a3
Habib Rahman Sherzad <sherzad.h@web.de>
2025-03-03 16:51:12 +0100

    add greetings-page

--- js-functions/js-functions_greeting-page/js/index.js
+++ js-functions/js-functions_greeting-page/js/index.js
@@ -23,11 +23,42 @@ Update the content and style of the page based on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
-  // Code here
+  const currentHour = new Date().getHours();
+
+  if (currentHour >= 6 && currentHour <= 12) {
+    return "Good Morning";
+  } else if (currentHour >= 13 && currentHour <= 18) {
+    return "Good Afternoon";
+  } else if (currentHour >= 19 && currentHour <= 22) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
+
+  // Alternative
+
+  // if (currentHour >= 6 && currentHour <= 12) return "Good Morning";
+  // if (currentHour >= 13 && currentHour <= 18) return "Good Afternoon";
+  // if (currentHour >= 19 && currentHour <= 22) return "Good Evening";
+  // return "Good Night";
 }
 
 function getDayColor() {
-  // Code here
+  const currentDay = new Date().getDay();
+
+  if (currentDay === 1) {
+    return "darkgray";
+  } else if (currentDay >= 2 && currentDay <= 5) {
+    return "lightblue";
+  } else {
+    return "hotpink";
+  }
+
+  // Alternative
+
+  // if (currentDay === 1) return "darkgray";
+  // if (currentDay >= 2 && currentDay <= 5) return "lightblue";
+  // return "hotpink";
 }
 
 display.textContent = getGreeting();
