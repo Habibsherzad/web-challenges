caa7ba44f7ad086a1dc7b0bf69ec4e58897d2ca2
Habib Rahman Sherzad <sherzad.h@web.de>
2025-03-10 16:44:58 +0100

    add js modern syntax back to basics

--- js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
+++ js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
@@ -3,15 +3,30 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
-export const getNameAndCountry = ({ name, country }) => [name, country];
+export function getNameAndCountry(city) {
+  return [city.name, city.country];
+}
 
-export const getRelocatedCity = (
-  city1,
-  city2 = { name: "Berlin", country: "Germany" }
-) => {
-  const [, country] = getNameAndCountry(city2);
-  return {
-    ...city1,
-    country,
-  };
-};
+export function getRelocatedCity(city1, city2) {
+  city2 = city2 || { name: "Berlin", country: "Germany" };
+
+  const country = getNameAndCountry(city2)[1];
+
+  const relocatedCity = {};
+
+  for (const key in city1) {
+    /*
+    💡 Hint
+    The `Object.hasOwn()` method ensures that only the direct properties of `city1` are
+    copied, excluding any inherited properties from the prototype chain.
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwn
+    */
+
+    if (Object.hasOwn(city1, key)) {
+      relocatedCity[key] = city1[key];
+    }
+  }
+
+  relocatedCity.country = country;
+  return relocatedCity;
+}
